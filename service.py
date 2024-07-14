c56eb8a3607d7600c0dd505f33a6b92487a1c326
AbsNone <absnone0@gmail.com>
2024-07-14 18:09:28 +0300

    FR added

--- service.py
+++ service.py
@@ -1,11 +1,18 @@
 from dataclasses import dataclass
+from enum import Enum
 
 
 FileName = str
 
 
+class FR(str, Enum):
+    CV2 = 'cv2'
+    MEDIA_PIPE = 'mediapipe'
+
 @dataclass
 class Face:
+    method: FR
+    label: str
     x: float
     y: float
     w: float
